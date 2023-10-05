1d836e445eb8205da80ed42b8854676baddc7459
unknown <thaitrongtindev@gmail.com>
2023-10-05 15:49:30 +0700

    call retrofit the first

--- app/src/main/java/com/example/tvshowsmall/activities/MainActivity.java
+++ app/src/main/java/com/example/tvshowsmall/activities/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.tvshowsmall.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.tvshowsmall.R;
+import com.example.tvshowsmall.viewmodels.MostPopularTVShowsViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MostPopularTVShowsViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        getMostPopularTVShows();
+    }
+
+    private void getMostPopularTVShows() {
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse
+                -> Toast.makeText(getApplicationContext(), "Total Pages" + mostPopularTVShowsResponse.getTotalPages(), Toast.LENGTH_SHORT).show());
     }
 }
