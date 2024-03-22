474c10c81c96175b75005a2c01f4a1b5fe647403
thaitrongtin <thaitrongtindev@gmail.com>
2024-03-22 16:30:24 +0700

    set up entity and dao:

---
+++ app/src/main/java/com/example/tvshowsmall/dao/TVShowDao.java
@@ -0,0 +1,26 @@
+package com.example.tvshowsmall.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.tvshowsmall.models.TVShow;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+@Dao
+public interface TVShowDao {
+
+    @Query("SELECT * FROM tvShows")
+    Flowable<List<TVShow>> getWatchList();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addToWatchList(TVShow tvShow);
+
+    @Delete
+    void removeFromWatchList(TVShow tvShow);
+}
